799d1db2980178b988cc90506e8152d334394293
victortpt <53759200+victortpt@users.noreply.github.com>
2019-10-03 14:04:17 +0200

    Update Register_View.php

--- Views/Register_View.php
+++ Views/Register_View.php
@@ -1,64 +1,96 @@
-	<!--
-Documento php que contiene la clase para la vista de registrarse.
-Pablo Pazos Domínguez, Alias:3w68x9
-02/10/2018
--->
-	<div id="caja">
-		<?php
 
-		class Register{
+<?php
+
+    class Register{
 
 
-			function __construct(){	
-				$this->render();
-			}
+        function __construct(){ 
+            $this->render();
+        }
 
-			function render(){
+        function render(){
 
-				include '../Views/Header.php'; //header necesita los strings
-		?>
-		<script type="text/javascript" src="../Views/Javascript/md5.js"></script> 
-		<script type="text/javascript">
-	    function encriptar(){
-	      document.getElementById('password').value = hex_md5(document.getElementById('password').value);
-	      return true;
-	    }
-	</script>
-	 	<section class="section_formulario">	
-	 		
-	 		
-			<form name = 'form2' id="form2" action='../Controllers/Register_Controller.php' enctype="multipart/form-data" method='post' onsubmit="return validarRegister(this); return encriptar();">
-				<fieldset>
-					<legend><?php echo $strings['Registro'];?></legend>
-				</fieldset>
-					
-				 	<input type = 'text' name = 'login' id = 'login' placeholder = 'DNI*' size = '9'  maxlength="9" onblur="comprobarDni(this);" >
-				 	<label><?php echo $strings['Sube una foto personal'];?>*</label>
-					<input type="file" name="file" id="fileToUpload">
-					<input type="password" id="password" name="password" placeholder="<?php echo $strings['Contraseña'];?>*" maxlength="20" size="34" onblur="comprobarTexto( this, 20 );">
-					<input type="text" id="nombre" name="nombre"  maxlength="30" size="37" placeholder="<?php echo $strings['Nombre'];?>*" onblur=" comprobarAlfabetico(this,30);">
-					<input type="text" id="apellidos" name="apellidos" placeholder="<?php echo $strings['Apellidos'];?>*" maxlength="50" size="74" onblur="comprobarAlfabetico(this,50);">
-					<input type="text" name="telefono" id="telefono" placeholder="<?php echo $strings['Teléfono'];?>*" maxlength="11" size="30" onblur="comprobarTelf(this);">
-					<label><?php echo $strings['Sexo'];?>*</label>
-					<select name="sexo">
-						<option value="hombre"><?php echo $strings['Hombre'];?></option>
-						<option value="mujer"><?php echo $strings['Mujer'];?></option>
-					</select>
-					<input type="email" id="email" name="email" placeholder="<?php echo $strings['Email'];?>*" maxlength="60" size="78" onblur="comprobarExpresionRegular(this, /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i, 60);">
-					<input type="text" id="fecha" class="tcal tcalInput" name="fecha" placeholder="<?php echo $strings['Fecha de Nacimiento'];?>*" readonly>
+            include '../Views/Header.php'; //header necesita los strings
+        ?>
 
-					<button type="submit"  class="button"><span class="fas fa-check"></span></button>
-					<a class="button2" href="../Controllers/Login_Controller.php"><i class="fas fa-undo" ></i></a>
-			</form>		
-			
-		</section>
-	
-</div> 			
-		<?php
-			include '../Views/Footer.php';
-		} //fin metodo render
+ 
+  <body>
+        <div>
+            <div class="cabecera color fixed-top">
+                <div class="row">
+                        <div class="col-sm-9">
+                            <img src="./SOURCE/LOGOaiDraif.png" style="height: 60px; width: 120px" >
+                            <div>
+                                
+                            </div>
+                        </div>
+                
 
-	} //fin REGISTER
+                    <div class="col-sm-3">
+                                <div class="float-right align-self-center row">
+                                    <div class="caja-login">
+                                        <button type="button" class="btn btn-outline-success color2">Login</button>
+                                    </div>
+                                    <div class="icono-imagen">
+                                        <span class="fas fa-language color2"></span>
+                                    </div>  
+                                </div>   
+                    </div>
+                </div>
+            </div>
+            
+            <div class="registro align-self-center ">
+                <div class="form-registro">
+                    <div class="regisro">
+                        <div class="col-sm-8">
+                            <h2 class="form-title" id="registro">Registro</h2>
+                            <div class="form-holder">
+                                <div class="col-sm-3">
+                                    <br><input type="text" class="input" placeholder="Login" /><br>
+                                </div>
+                                <div class="col-sm-3">
+                                    <br><input type="text" class="input" placeholder="Nombre" /><br>
+                                </div>
+                                <div class="col-sm-3">
+                                    <br><input type="text" class="input" placeholder="Apellidos" /><br>
+                                </div>
+                                <br><input type="email" class="input" placeholder="Email" /><br>
+                                <br><input type="password" class="input" placeholder="Password" /><br>
+                            </div>
+                        </div>
+                        <div class="col-2">
+                                <div class="input-select">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                       <br> <select name="gender" id="selectGrupo">
+                                            <option disabled="disabled" selected="selected">¿Para que va a utilzar este servicio?</option>
+                                            <option>Uso profesional</option>
+                                            <option>Uso personal</option>
+                                            <option>Uso educativo</option>
+                                            <option>Otro</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <br><div class="form-check">
+                          <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                          <label class="form-check-label" for="defaultCheck1">
+                            ¿Acepta los términos y condiciones?
+                          </label>
+                        </div>
+                        <br><div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit" id="btnRegistro"><a href="../Controllers/Register_Controller.php?" class="btn btn-outline-success color2">Registrarse</a></button>
+                        </div>
+                    </div>
+                </div>
+
+<a href='../Controller/Login_Controller.php'>Volver </a>
+        
+        <?php
+            include '../Views/Footer.php';
+        } //fin metodo render
+
+    } //fin REGISTER
 
 ?>
-
